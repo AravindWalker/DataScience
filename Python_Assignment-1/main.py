fd0121fbac99d6db86d089d50da34293283ca26f
AravindTech <99093930+AravindWalker@users.noreply.github.com>
2022-07-28 21:44:00 +0530

    Added comments to code

--- Python_Assignment-1/main.py
+++ Python_Assignment-1/main.py
@@ -1,6 +1,6 @@
-from loginhelper import LoginHelp
+from loginhelper import LoginHelp # import modules that has login/register supporting code
 print("Enter 'Login' or 'Register'")
 choice = input("Enter your choice: ")
 mail_id = input("Enter the mail-ID: ")
-user = LoginHelp(choice, mail_id)
+user = LoginHelp(choice, mail_id) # initializing the LoginHelp class
 user.control()
