0c1dea055eb4163640745dec251b5948a9b6dec8
AravindTech <99093930+AravindWalker@users.noreply.github.com>
2022-11-05 19:30:39 +0530

    Update main.py

--- MongoDB Assignment-5/Telephone_directory/main.py
+++ MongoDB Assignment-5/Telephone_directory/main.py
@@ -1,6 +1,20 @@
 from TD import TelDirectory
 
+serv_id = input("Please enter telephone server that you need to connect: ")
 print("These are the things you can do with telephone directory: ")
-print("1. CREATE", "2. READ", "3. UPDATE", "4. DELETE", sep="\n")
-ch = input("Enter what you need in telephone directory: ")
-action = TelDirectory(ch)
+
+opts = ["Create", "Read", "Update", "Delete"]
+
+for i, j in enumerate(opts):
+    print(i + 1, ". ", j)
+
+check = 0
+
+while check == 0:
+    ch = input("Enter what you need in telephone directory: ")
+    if ch in opts:
+        check = 1
+    else:
+        print("Please select the above mentioned options")
+
+action = TelDirectory(serv_id, ch)
